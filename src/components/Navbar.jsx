dfaba22c2862db3f8327a19a3824dbd54c9d0083
Stoyan Ivanov <ivanov.stoyan100@gmail.com>
2026-02-03 21:57:27 +0000

    Added a logo and footer (WIP)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
+import logo from '../assets/logo.png';
 
 export default function Navbar() {
 	return (
 		<nav className={styles.nav}>
-			<h1 className={styles.logo}>Пазарджик Трансит</h1>
+			<Link to="/">
+				<img src={logo} alt="Пазарджик Трансит" className={styles.logo} />
+			</Link>
 			<div className={styles.linksContainer}>
 				<Link to="/" className={styles.link}>
 					Начало
