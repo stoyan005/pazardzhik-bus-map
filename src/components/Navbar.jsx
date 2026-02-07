030537a4bba76cde33a1246c70a8d5d7fedbf159
Stoyan Ivanov <ivanov.stoyan100@gmail.com>
2026-02-07 21:59:52 +0000

    Added a hamburger menu to the mobile devices and smaller screens, also changed the colors of the trolleybuses and fixed the responsiveness of the home page.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,18 @@
+import { useState } from 'react';
+import { Sling as Hamburger } from 'hamburger-react';
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
 import logo from '../assets/logo.png';
 
 export default function Navbar() {
+	const [menuOpen, setMenuOpen] = useState(false);
+
 	return (
 		<nav className={styles.nav}>
 			<Link to="/">
 				<img src={logo} alt="Пазарджик Трансит" className={styles.logo} />
 			</Link>
+
 			<div className={styles.linksContainer}>
 				<Link to="/" className={styles.link}>
 					Начало
@@ -19,6 +24,32 @@ export default function Navbar() {
 					Карта за тролей
 				</Link>
 			</div>
+
+			<div className={styles.hamburger}>
+				<Hamburger toggled={menuOpen} toggle={setMenuOpen} />
+			</div>
+
+			{menuOpen && (
+				<div className={styles.mobileMenu}>
+					<Link to="/" className={styles.link} onClick={() => setMenuOpen(false)}>
+						Начало
+					</Link>
+					<Link
+						to="/bus-map"
+						className={styles.link}
+						onClick={() => setMenuOpen(false)}
+					>
+						Карта за автобуси
+					</Link>
+					<Link
+						to="/trolley-map"
+						className={styles.link}
+						onClick={() => setMenuOpen(false)}
+					>
+						Карта за тролей
+					</Link>
+				</div>
+			)}
 		</nav>
 	);
 }
