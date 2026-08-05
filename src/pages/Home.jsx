e4644fbbe5df4e7ca6cf8baec4a1f8a7cc1b5613
Stoyan Ivanov <ivanov.stoyan100@gmail.com>
2026-08-05 21:10:54 +0100

    Updated an asset to display another message on the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,7 +37,7 @@ export default function Home() {
 									<path d="M9 19h8.5a3.5 3.5 0 0 0 0-7h-11a3.5 3.5 0 0 1 0-7H15" />
 									<circle cx="18" cy="5" r="3" />
 								</svg>
-								Потребители на достъп
+								Улеснено пътуване
 							</p>
 
 							<p>
